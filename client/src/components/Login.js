7946a6d8b6d531cd3f113686f895fe1e8a51695a
Wendy You <wyou130@gmail.com>
2022-10-04 11:50:23 -0400

    Log in form and current user state set up

---
+++ client/src/components/Login.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+// import { useHistory } from 'react-router-dom'
+
+function Login({ onLogIn }) {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    // let history = useHistory()
+
+    // function showPassword(password) {
+    //     if (password.type === "password") {
+    //         password.type = "text"
+    //     } else {
+    //         password.type = "password"
+    //     }
+    // }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        let loginInput = {
+            email: email,
+            password: password
+        }
+        // console.log(loginInput)
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(loginInput)
+        })
+            .then(res => res.json())
+            .then(loggedInUser => onLogIn(loggedInUser))
+        // history.push('/')
+        setEmail("")
+        setPassword("")
+    }
+
+    return (
+        <div>
+            <p>Log In</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input 
+                    required 
+                    type="text" 
+                    name="email" 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                    required 
+                    type="password" 
+                    name="password" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                {/* <input type="checkbox" onClick={showPassword}>Show Password</input> */}
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login 

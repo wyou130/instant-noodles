a1e618e182fd5c39f853dac453f88301f91eb2ee
Wendy You <wyou130@gmail.com>
2022-10-04 13:34:30 -0400

    Log out logic complete, NavBar set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::API
 
     before_action :authenticated_user
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
-    end
+    # def hello_world
+    #     session[:count] = (session[:count] || 0) + 1
+    #     render json: { count: session[:count] }
+    # end
 
     private
 
     def authenticated_user
         @current_user ||= User.find_by(id: session[:user_id])
+        # debugger
         render json: { error: "Not authorized"}, status: :unauthorized unless @current_user 
     end
 

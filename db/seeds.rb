059a915d61536928f730f2b238ecced2fb3e681e
Wendy You <wyou130@gmail.com>
2022-10-06 14:11:46 -0400

    Images rendered in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,21 @@
 
 # Users
 puts "Seeding users..."
-naijia = User.create(name: "NinjaH", location: "NY", password: "iLuvN00dl3z", email: "huang.naijia@gmail.com")
-wendy = User.create(name: "WendayY", location: "NY", password: "iLuvN00dl3z2", email: "wyou130@gmail.com")
-ethan = User.create(name: "EshanB", location: "NY", password: "iH8N00dl3z", email: "ebb0202@aol.com")
+naijia = User.create(name: "NinjaH", location: "NY", password: "iLuvN00dl3z", email: "huang.naijia@gmail.com", image: "https://img.freepik.com/free-vector/noodle-egg-with-chopstick-cartoon-icon-illustration_138676-2662.jpg?w=2000")
+wendy = User.create(name: "WendayY", location: "NY", password: "iLuvN00dl3z2", email: "wyou130@gmail.com", image: "https://img.freepik.com/free-vector/french-fries-cartoon-icon-illustration-fast-food-icon-concept-isolated-flat-cartoon-style_138676-2923.jpg?w=2000")
+ethan = User.create(name: "EshanB", location: "NY", password: "iH8N00dl3z", email: "ebb0202@aol.com", image: "https://img.freepik.com/premium-vector/pizza-illustration-slice-pizza-illustration-isolated-cute-pizza-icon-illustration_597063-104.jpg?w=2000")
 
 # Noodles
 puts "Seeding noodles..."
-ohsunghong = Noodle.create(brand: "Oh Sung Hong", flavor: "Hot & Spicy", image: "https://lh3.googleusercontent.com/keep-bbsk/AP6BvTRZRmppUyg5nFXzum8s2IsCElLZ6QUWJqgCt5_sRuXrcD93VdXyHmEE-IGxn38wSVYAUV4kXBFeoHqWr9GyeFX1IoLFCJ03KnaIGaeFOI5MyE1X=s544", birthplace: "Korea", style: "Noodle Soup")
+ohsunghong = Noodle.create(brand: "Oh Sung Hong", flavor: "Hot & Spicy", image: "../oh-sung-hong.jpg", birthplace: "Korea", style: "Noodle Soup")
 
-ottogi = Noodle.create(brand: "Ottogi", flavor: "Odongtong Myoni Seafood", image: "https://lh3.googleusercontent.com/keep-bbsk/AP6BvTRBCt2y_EGwkv-jjX-EykDBgMoi-hnxUeJHpI7C6CB4NNeYIg88r1BSMPUb3zO67TTIwPzUL995Cycs_R5hk5GnWZ9vnokm5LhufrLzh8f3Ejjj=s600", birthplace: "Korea", style: "Noodle Soup")
+ottogi = Noodle.create(brand: "Ottogi", flavor: "Odongtong Myoni Seafood", image: "../ottogi-odongtong.jpg", birthplace: "Korea", style: "Noodle Soup")
 
-ottogi2 = Noodle.create(brand: "Ottogi", flavor: "Sesame", image: "https://lh3.googleusercontent.com/keep-bbsk/AP6BvTRC01QBk2qgpcKdqfVvkNQbUupSdGMPuU06VSVKXI2ZK4l3Rl5lRaKtSlMlQFHqwMKKee5yCium7nv9TJkcnTn3wKmooShn7ydzS52PlaVOqGRd=s577", birthplace: "Korea", style: "Noodle Soup")
+ottogi2 = Noodle.create(brand: "Ottogi", flavor: "Sesame", image: "../ottogi-sesame.jpeg", birthplace: "Korea", style: "Noodle Soup")
+
+ottogi3 = Noodle.create(brand: "Ottogi", flavor: "Yeul", image: "../ottogi-yeul.jpeg", birthplace: "Korea", style: "Noodle Soup")
+
+nongshim = Noodle.create(brand: "Nongshim", flavor: "Ansung", image: "../nongshim-ansung.jpeg", birthplace: "USA", style: "Noodle Soup")
 
 # Reviews
 puts "Seeding reviews..."
@@ -27,5 +31,9 @@ Review.create(noodle_rating: 4, noodle_comment: "Surprisingly springy and withst
 Review.create(noodle_rating: 3, noodle_comment: "Udon isn’t my favorite noodle but tried to be objective. In the first few minutes, the noodles withstood soaking quite well and remained bouncy. However, towards the end they did start to swell although avoided breaking down did lose some of the original texture.", toppings_rating: 3, toppings_comment: "Standard toppings but with fishcakes, seaweed AND a piece of kombu! Would recommend putting kombu into the pot at the start of the boil, and rest of the toppings closer to the end of the boil to retain more flavor", spice_rating: 1.5, overall_rating: 3, overall_comment: "A well balanced bowl of noodles worthy of challenging Neoguri, with potential to be great with some tweaks.", user_id: naijia.id, noodle_id: ottogi.id)
 
 Review.create(noodle_rating: 3.5, noodle_comment: "Generally favorable noodles with springy texture. Not too chewy and erred slightly softer but overall solid and did not degrade much with soaking.", toppings_rating: 1.5, toppings_comment: "I had high hopes but was ultimately disappointed by the egg block. It was very confusing whether it was meant to be broken up or eaten as a whole unit. I split it in half and did both. Eating the block was confusing at best, so I think it was meant to create an egg drop soup texture, which would have helped the broth score… ever so slightly. Even still, the egg block was not big enough to make a difference. Very little other veggies and there were only a few sesame seeds.", spice_rating: 1.5, spice_comment: "Interesting spice level due to the sesame chili oil, which hit the lips more but the tongue less than other ramen.", overall_rating: 2, overall_comment: "Not the worse I've had but certainly wouldn't recommend it. Highlight was the sesame chili oil.", user_id: naijia.id, noodle_id: ottogi2.id)
+
+Review.create(noodle_rating: 3.5, noodle_comment: "Pretty much the same as the other Ottogi brand ramen (square block) that was above average, nice and chewy", toppings_rating: 2, toppings_comment: "The only reason why this is not lower is due to the toppings being somewhat “fresher” and better tasting than usual, although it still lacks the large chunks and freshness of Shin. It did, however, add a bit to the broth score since it contributed a bit to the overall flavor of the soup.", spice_rating: 2, overall_rating: 3.5, overall_comment: "Ok, cheated a little bit here due to it having slightly more spice flavor. It was the only thing that edged it a little bit over Oh Sung. The flavor and overall experience were also more similar to Shin ramen.", user_id: naijia.id, noodle_id: ottogi3.id)
+
+Review.create(noodle_rating: 1.5, noodle_comment: "These noodles are really disappointing compared to what I expected from NongShim. They did not have a good texture at the start and are squarish rather than perfectly round.", toppings_rating: 1.5, toppings_comment: "Toppings are few despite the call-out of mushrooms on the front. The rest of the toppings were mostly small pieces of seaweed which is extremely odd for a beef noodle soup.", spice_rating: 1, overall_rating: 1.5, overall_comment: "Underwhelming and very disappointing. But mostly just a very confusing product that shouldn't exist.", user_id: naijia.id, noodle_id: nongshim.id)
 
 puts "Done!"
